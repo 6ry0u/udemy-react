a5e2dd882ed6c0d99d7242a36fb5d43209d17735
Gry0u <gry0u@tutanota.com>
2019-04-28 17:52:19 +0200

    Init seasons app (Section 4)

---
+++ seasons/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import SeasonDisplay from './SeasonDisplay'
+
+const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    position => console.log(position), // success callback
+    err => console.log(err)
+  )
+  return (
+    <div>Hi There</div>
+  )
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
