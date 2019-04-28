c232b12646710919af65f2a81936832c5bb6211b
Gry0u <gry0u@tutanota.com>
2019-04-28 19:42:17 +0200

    feat: init and update state

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -3,13 +3,26 @@ import ReactDOM from 'react-dom'
 import SeasonDisplay from './SeasonDisplay'
 
 class App extends React.Component {
-  render() {
+  // not required by React
+  constructor(props) {
+    super(props) // pass props to Component parent class constructor
+
+    // init state
+    this.state = { lat: null }
     window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position), // success callback
+      position => {
+        // UPDATE SET WITH SETSTATE
+        this.setState({ lat: position.coords.latitude })
+      }, // success callback
       err => console.log(err)
     )
+  }
+
+
+  // Required by React
+  render() {
     return (
-      <div>Latitude: </div>
+      <div>Latitude: {this.state.lat}</div>
     )
   }
 }
