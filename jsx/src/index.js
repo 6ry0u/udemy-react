ed34aeec7a9eb0be0068d941ab66182450b78af5
Gry0u <gry0u@tutanota.com>
2019-04-28 14:38:44 +0200

    Finish Section 2

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,14 +1,18 @@
 // import react and reactDOM libraries
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react"
+import ReactDOM from "react-dom"
 
+const buttonText = 'Click on me'
 // create react component
 const App = () => {
-  return <div>Hi there!</div>
+  return (
+    <div>
+      <label className="label" htmlFor="name">Enter name:</label>
+      <input id="name" type="text"/>
+      <button style={{backgroundColor:'blue', color: 'white'}}>{buttonText}</button>
+    </div>
+  )
 }
 
 // take the react component and show it on the screen
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
-)
+ReactDOM.render(<App />, document.querySelector("#root"))
