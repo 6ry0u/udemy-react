68d297561fb56df561b89f42d5f79265e6119d42
Gry0u <gry0u@tutanota.com>
2019-04-28 20:40:10 +0200

    feat: Display season

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -4,27 +4,40 @@ import SeasonDisplay from './SeasonDisplay'
 
 class App extends React.Component {
   // not required by React
-  constructor(props) {
-    super(props) // pass props to Component parent class constructor
+  // constructor(props) {
+  //   super(props) // pass props to Component parent class constructor
+  //
+  //   // init state
+  //   this.state = { lat: null, errMessage: '' }
+  // }
 
-    // init state
-    this.state = { lat: null, errMessage: '' }
+// Alternate init (babel creates the constructor for us under the hood and init with this.state = ...)
+  state = { lat: null, errMessage: '' }
+
+  // called 1 time when the component is rendered for the first time
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        // UPDATE SET WITH SETSTATE
-        this.setState({ lat: position.coords.latitude })
-      }, // success callback
+      position => this.setState({ lat: position.coords.latitude }),
       err => this.setState({errMessage: err.message})
     )
   }
 
+  // called every time component is updated (when setState is called)
+  componentDidUpdate() {
+    console.log('My Component was just updated and rerendered')
+  }
+
+  // Called when we stop rendering the component
+  // componentWillUnmount() {
+
+  // }
 
   // Required by React
   render() {
     if (this.state.errMessage && !this.state.lat) {
       return <div>Error: {this.state.errMessage}</div>
     } else if (!this.state.errMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>
+      return <SeasonDisplay lat={this.state.lat}/>
     } else {
       return <div>Loading...</div>
     }
