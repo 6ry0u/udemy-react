f08366fc8e488fe98de5d51891140ef928b101a4
Gry0u <gry0u@tutanota.com>
2019-05-04 15:38:34 +0200

    Finish Section 10

--- pics/src/components/ImagesList.js
+++ pics/src/components/ImagesList.js
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import './ImagesList.css'
+import ImageCard from './ImageCard'
 const ImagesList = props => {
-    const images = props.images.map(({ description, id, urls }) => {
-      return <img src={urls.regular} key={id} alt={description}/>
+    const images = props.images.map((image) => {
+      return <ImageCard key={image.id} image={image} />
     })
     return (
-      <div>{images}</div>
+      <div className="images-list">{images}</div>
     )
 }
 
