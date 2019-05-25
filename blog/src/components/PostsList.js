049138c014c999e4f6e8135e5b7760e71dc01256
Gry0u <gry0u@pop-os.localdomain>
2019-05-25 21:11:11 +0200

    Update postsReducers

--- blog/src/components/PostsList.js
+++ blog/src/components/PostsList.js
@@ -8,10 +8,17 @@ class PostsList extends Component {
   }
 
   render () {
+    console.log(this.props.posts)
     return (
       <div>Posts List</div>
     )
   }
 }
 
-export default connect(null, { fetchPosts })(PostsList)
+const mapStateToProps = (state) => {
+  return { posts : state.posts }
+}
+export default connect(
+  mapStateToProps,
+  { fetchPosts }
+)(PostsList)
