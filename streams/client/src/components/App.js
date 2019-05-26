2eb8e48c2f25bd0045ad605ee21e04a3815d2743
Gry0u <gry0u@tutanota.com>
2019-05-26 15:14:36 +0200

    Add first routes

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
 
+const PageOne = () => {
+  return <div>PageOne</div>
+}
+const PageTwo = () => {
+  return (
+    <div>PageTwo
+      <button>Click</button>
+    </div>
+  )
+}
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+        <Route path="/" exact component={PageOne} />
+        <Route path="/pagetwo" component={PageTwo} />
+      </BrowserRouter>
+    </div>
   )
 }
 
