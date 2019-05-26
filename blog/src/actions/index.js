23a2e7a732cfd7bd8693b272781256375f64e8aa
Gry0u <gry0u@pop-os.localdomain>
2019-05-26 14:25:51 +0200

    Refactor with FetchPostsAndUsers

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,6 +1,13 @@
 import jsonPlaceholder from '../api'
 import _ from 'lodash'
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts())
+
+  const userIds = _.uniq(_.map(getState().posts, 'userId'))
+  userIds.forEach(id => dispatch(fetchUser(id)))
+}
+
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts')
     dispatch({
@@ -9,13 +16,19 @@ export const fetchPosts = () => async dispatch => {
     })
   }
 
-export const fetchUser = id => dispatch => _fetchUser(id, dispatch)
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch)
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`)
+//   dispatch({
+//     type: 'FETCH_USER',
+//     payload: response.data
+//   })
+// })
 
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = id => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`)
   dispatch({
     type: 'FETCH_USER',
     payload: response.data
   })
-})
+}
