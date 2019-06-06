0f22955e96fbce8043264deb0aa88ebc6db065ee
Gry0u <gry0u@tutanota.com>
2019-06-06 22:02:46 +0200

    feat: Can edit stream

--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -1,11 +1,20 @@
+import _ from 'lodash'
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-import { fetchStream } from '../../actions'
+import { fetchStream, editStream } from '../../actions'
+import StreamForm from './StreamForm'
 
 class StreamEdit extends Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id)
+  }
+
+  onSubmit = formValues => {
+    this.props.editStream(
+      this.props.match.params.id,
+      formValues
+    )
   }
 
   render() {
@@ -15,7 +24,13 @@ class StreamEdit extends Component {
       )
     } else {
       return (
-        <div>{this.props.stream.title}</div>
+        <div>
+          <h3>Edit Stream</h3>
+          <StreamForm
+          onSubmit={this.onSubmit}
+          initialValues={_.pick(this.props.stream, 'title', 'description')}
+          />
+        </div>
       )
     }
   }
@@ -24,4 +39,4 @@ class StreamEdit extends Component {
 const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] }
 }
-export default connect(mapStateToProps, { fetchStream })(StreamEdit)
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit)
