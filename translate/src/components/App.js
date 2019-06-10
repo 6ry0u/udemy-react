a0d9a6efec2991dc68fa8b481374192b79090b3f
Gry0u <gry0u@tutanota.com>
2019-06-10 12:03:48 +0200

    Scaffold project section 22. Set up Mark Up

---
+++ translate/src/components/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import UserCreate from './UserCreate'
+
+class App extends Component {
+  state = { language: 'english' }
+
+  onLanguageChange = language => {
+    this.setState({ language })
+  }
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>Select Language:
+          <i
+          className="flag us"
+          onClick={() => this.onLanguageChange('english')}
+          />
+          <i
+          className="flag fr"
+          onClick={() => this.onLanguageChange('french')}
+          />
+        </div>
+        <UserCreate />
+      </div>
+    )
+  }
+}
+
+export default App
