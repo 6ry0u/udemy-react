1756287368768261ae243ec3ad445ce7bbfa6bbd
6ry0u <6ry0u@tutanota.com>
2019-11-24 14:40:38 +0100

    refactor: Use Hook instead of class Component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,53 +1,34 @@
-import './index.css'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import SeasonDisplay from './SeasonDisplay'
 import Loader from './Loader'
 
-class App extends React.Component {
-  // not required by React
-  // constructor(props) {
-  //   super(props) // pass props to Component parent class constructor
-  //
-  //   // init state
-  //   this.state = { lat: null, errMessage: '' }
-  // }
+import './index.css'
 
-// Alternate init (babel creates the constructor for us under the hood and init with this.state = ...)
-  state = { lat: null, errMessage: '' }
+const App = () => {
+  const [lat, setLat] = useState(null)
+  const [error, setError] = useState('')
 
-  // called 1 time when the component is rendered for the first time
-  componentDidMount() {
+  useEffect(() => {
     window.navigator.geolocation.getCurrentPosition(
-      position => this.setState({ lat: position.coords.latitude }),
-      err => this.setState({errMessage: err.message})
+      position => setLat(position.coords.latitude),
+      err => setError(err.message)
     )
-  }
+  }, [])
 
-  // called every time component is updated (when setState is called)
-  componentDidUpdate() {
-    console.log('My Component was just updated and rerendered')
-  }
-
-  // Called when we stop rendering the component
-  // componentWillUnmount() {
-
-  // }
-
-  renderContent() {
-    if (this.state.errMessage && !this.state.lat) {
-      return <div>Error: {this.state.errMessage}</div>
-    } else if (!this.state.errMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat}/>
+  const renderContent = () => {
+    if (error && !lat) {
+      return <div>Error: {error}</div>
+    } else if (!error && lat) {
+      return <SeasonDisplay lat={lat} />
     } else {
-      return <Loader message="Waiting for user to allow or block fetching location"/>
+      return <Loader message='Waiting for user to allow or block fetching location' />
     }
   }
 
-  // Required by React
-  render() {
-    return <div className="red border">{this.renderContent()}</div>
-  }
+  return (
+    <div className='red border'>{renderContent()}</div>
+  )
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'))
